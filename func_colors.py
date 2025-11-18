abb4cd529b7d9bad68f3a6d8ccf034671fefaace
kanghg0106 <kanghg0106@gmail.com>
2025-11-18 20:22:29 +0900

    Initial commit

--- func_colors.py
+++ func_colors.py
@@ -1,15 +1,6 @@
-from numpy import linspace, array
-from matplotlib.pyplot import figure, imread, imshow, axis, show
 from cmcrameri import cm
 from matplotlib.pyplot import get_cmap
-
-
-def help_colormap():
-    figure(dpi=600)
-    help_image = imread('C:/Users/kangh/Documents/RESEARCH/Functions/Crameri colormaps.png')
-    imshow(help_image)
-    axis('off')
-    show()
+from numpy import linspace
 
 
 def get_color_array(cmap, data_length, c_start=0.075, c_end=1):
@@ -24,12 +15,14 @@ def rgb2hex(color_array):
     r, g, b = round(color_array[0]), round(color_array[1]), round(color_array[2])
     return f"#{r:02x}{g:02x}{b:02x}"
 
-c_blue, c_red = get_color_array('berlin', 2, c_start=0.2,c_end=0.87)
-c_magenta, c_green = get_color_array('bam', 2, c_start=0.2,c_end=0.9)
-_, c_yellow = get_color_array('bamako', 2, c_start=0.2,c_end=0.9)
 
-c_red = rgb2hex(c_red)
-c_blue = rgb2hex(c_blue)
-c_green = rgb2hex(c_green)
-c_magenta = rgb2hex(c_magenta)
-c_yellow = rgb2hex(c_yellow)
+c_red = "#FF2C43"
+c_orange = "#FF9A47"
+c_yellow = "#F8E448"
+c_lime = "#98EE84"
+c_green = "#1B9C21"
+c_mint = "#76DDBE"
+c_blue = "#49BCE6"
+c_navy = "#4965E6"
+c_purple = "#A588DB"
+c_pink = "#FF7BCC"
